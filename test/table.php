a126ec67eca4496e3c4e03deb41b3442d93e2811
Cam Farquharson <fuller.igames@gmail.com>
2017-06-22 10:57:22 -0400

    Add files via upload

---
+++ test/table.php
@@ -0,0 +1,11 @@
+<?php
+  var theData = "hello world";
+   var theData = "hello world";
+    var theData = "hello world";
+     var theData = "hello world";
+  echo "<table>
+          <tr>
+            <td>$theData</td>
+          </tr>
+        </table>";
+?>
