f981ebe8dbbe35f8cdd703c2411898b0e2f4d868
Cam Farquharson <fuller.igames@gmail.com>
2017-07-10 15:38:00 -0400

    Revert "Add files via upload"
    This reverts commit b785b516683c4592d1e99b078e3c91a33d2fae00.

--- test/table2.php
+++ test/table2.php
@@ -1,5 +1,8 @@
 <?php
   var theData = "hello world";
+   var theData = "hello world";
+    var theData = "hello world";
+     var theData = "hello world";
   echo "<table>
           <tr>
             <td>$theData</td>
